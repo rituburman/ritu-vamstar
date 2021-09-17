06bf081a8d44650ae8ac99c75d2fd4d535c5efc2
Ritu Burman <ritu.burman415@gmail.com>
2021-09-17 21:09:56 +0530

    Update code-20210909-rituburman.js

--- code-20210909-rituburman.js
+++ code-20210909-rituburman.js
@@ -9,44 +9,6 @@ const data = [{ "Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, {
     "HeightCm": 167, "WeightKg": 82
 }];
 
-let overWeight = 0;
-function BMI() {
-
-    this.bmi = Math.round(this.WeightKg / (this.HeightCm / 100 * this.HeightCm / 100));
-
-
-    if (this.bmi <= 18.4) {
-        this.bmiCategory = "Underweight";
-        this.healthRisk = "Malnutrition Risk";
-    } else if (this.bmi <= 24.9) {
-        this.bmiCategory = "Normal Weight";
-        this.healthRisk = "Low Risk";
-    } else if (this.bmi <= 29.9) {
-        this.bmiCategory = "Overweight";
-        this.healthRisk = "Enhanced Risk";
-        overWeight++;
-    } else if (this.bmi <= 34.9) {
-        this.bmiCategory = "Moderatly Obese";
-        this.healthRisk = "Medium Risk";
-        overWeight++;
-    } else if (this.bmi <= 39.9) {
-        this.bmiCategory = "Severly Obese";
-        this.healthRisk = "High Risk";
-        overWeight++;
-    } else {
-        this.bmiCategory = "Very Severly Obese";
-        this.healthRisk = "Very High Risk";
-        overWeight++;
-    }
-
-}
-
-for (let person of data) {
-    BMI.apply(person);
-    console.log(person);
-}
-console.log("Total Overweight: " + overWeight);
-
 
 
 
